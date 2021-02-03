fcee0b6ea6a583ba04e6b781bb8f415ddbb6b8ea
saluja717 <68748154+saluja717@users.noreply.github.com>
2021-02-03 12:37:55 +0530

    Add files via upload

---
+++ Permutation.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void find(string s,int l,int r)
+{
+	if(l==r)
+	{
+		cout<<s<<endl;
+		return;
+	}
+	for(int i=l;i<=r;i++)
+	{
+		swap(s[i],s[l]);
+		find(s,l+1,r);
+		swap(s[i],s[l]);
+	}
+	return;
+}
+int main()
+{
+	string s;
+	cin>>s;
+	find(s,0,s.length()-1);
+	return 0;
+}
